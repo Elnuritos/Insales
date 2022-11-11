93021eeb5929c4820c79a6c79286788e0d541852
elnuritos <elnu1990@list.ru>
2022-11-11 16:46:58 +0300

    UpdateCustomStatus added

--- src/Traits/OrderTrait.php
+++ src/Traits/OrderTrait.php
@@ -5,7 +5,8 @@ namespace Insales\Traits;
 use Insales\Abstracts\Entity;
 use Insales\library\Insales;
 
-trait OrderTrait {
+trait OrderTrait
+{
 
     /**
      * Создание заказа
@@ -14,9 +15,9 @@ trait OrderTrait {
      * @param array $order Массив заказа в соответствии с документацией
      * @return \Insales\Abstracts\Entity Ответ от сервера
      */
-    public function createOrder(array $order) : Entity
+    public function createOrder(array $order): Entity
     {
-        
+
         return $this->client->executeCreateRequest(
             $this->generateUrl(self::API_URL_ORDERS),
             $order
@@ -29,9 +30,9 @@ trait OrderTrait {
      * @param array $order
      * @return \Insales\Abstracts\Entity
      */
-    public function editOrder(int $id, array $order) : Entity
+    public function editOrder(int $id, array $order): Entity
     {
-        
+
         return $this->client->executeUpdateRequest($this->generateUrl(self::API_URL_ORDERS, $id), $order);
     }
 
@@ -40,9 +41,9 @@ trait OrderTrait {
      * @param array $params Доступные параметры запроса
      * @return \Insales\Abstracts\Entity
      */
-    public function getOrders(array $params = array()) : Entity
+    public function getOrders(array $params = array()): Entity
     {
-        
+
         return $this->client->executeListRequest($this->generateUrl(self::API_URL_ORDERS), $params);
     }
 
@@ -51,9 +52,9 @@ trait OrderTrait {
      * @param string|integer $id Идентификатор заказа
      * @return \Insales\Abstracts\Entity
      */
-    public function getOrderById(int $id) : Entity
+    public function getOrderById(int $id): Entity
     {
-        
+
         return $this->client->executeGetRequest(
             $this->generateUrl(self::API_URL_ORDERS, $id),
             $id
@@ -64,9 +65,9 @@ trait OrderTrait {
      * Получает количество заказов
      * @return \Insales\Abstracts\Entity Количество заказов
      */
-    public function getOrdersCount() : Entity
+    public function getOrdersCount(): Entity
     {
-        
+
         $response = $this->request(
             $this::METHOD_GET,
             self::API_URL . self::API_URL_ORDERS . '/count.json'
@@ -79,46 +80,60 @@ trait OrderTrait {
      * @param int $id Идентификатор заказа
      * @return \Insales\Abstracts\Entity
      */
-    public function removeOrder(int $id) : Entity
+    public function removeOrder(int $id): Entity
     {
-        
+
         return $this->client->executeRemoveRequest(
             $this->generateUrl(self::API_URL_ORDERS, $id),
             $id
         );
     }
 
-    public function updateOrder(int $id, array $data) : Entity
+    public function updateOrder(int $id, array $data): Entity
     {
-         
+
         return $this->client->executeUpdateRequest(
             $this->generateUrl(self::API_URL_ORDERS, $id),
             $data
         );
     }
-      /**
+    /**
      * Создание корзины заказа по идентификатору
      * @param int $id Идентификатор заказа
      * @param array $data Параметры
      * @return \Insales\Abstracts\Entity
      */
-    public function createOrderLineByProduct(int $id, array $data) : Entity
+    public function createOrderLineByProduct(int $id, array $data): Entity
     {
-         
+
         return $this->client->executeUpdateRequest(
             $this->generateUrl(self::API_URL_ORDERS, $id),
             $data
         );
     }
-      /**
+    /**
      * Удаление корзины заказа по идентификатору
      * @param int $id Идентификатор заказа
      * @param array $data Параметры
      * @return \Insales\Abstracts\Entity
      */
-    public function removeOrderLine(int $id,array $data) : Entity
+    public function removeOrderLine(int $id, array $data): Entity
     {
-        
+
+        return $this->client->executeUpdateRequest(
+            $this->generateUrl(self::API_URL_ORDERS, $id),
+            $data
+        );
+    }
+    /**
+     * Обновление статуса заказа по идентификатору
+     * @param int $id Идентификатор заказа
+     * @param array $data Параметры
+     * @return \Insales\Abstracts\Entity
+     */
+    public function UpdateCustomStatus(int $id, array $data): Entity
+    {
+
         return $this->client->executeUpdateRequest(
             $this->generateUrl(self::API_URL_ORDERS, $id),
             $data
